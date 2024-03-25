5da1adbea6bbba739a2db39f74c8ecce9720a2da
stefanBid <biddau.stefano99@gmail.com>
2024-03-25 11:59:16 +0100

    setup heroicons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 
+import { XMarkIcon } from '@heroicons/vue/24/solid';
+
 </script>
 
 <template>
   <div class="text-red-500 h-[4000px] overflow-y-auto">
     My Portfolio
+    <XMarkIcon class="w-6 h-6" />
   </div>
 </template>
