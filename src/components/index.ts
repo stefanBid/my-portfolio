a7355f474a8e218dbed4880f2b9e83f554955747
stefanBid <biddau.stefano99@gmail.com>
2024-03-27 03:24:31 +0100

    create cover for Home-Page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 // ATOMS
 export { default as PageHeading } from '@/components/atoms/page-heading/PageHeading.vue';
-
+export { default as PageCover } from '@/components/atoms/page-cover/PageCover.vue';
 // MOLECULES
 
 // ORGANISMS
--- src/views/home-page/HomePage.vue
+++ src/views/home-page/HomePage.vue
@@ -1,16 +1,99 @@
 <script setup lang="ts">
+import { PageCover } from '@/components';
+import backgroundClip from '@/assets/coverVideo.mp4';
+import { onMounted, ref } from 'vue';
 
+import { useGlobalBreakpoints } from '@/hooks';
+
+const { xs, sm, md } = useGlobalBreakpoints();
+
+// Feature 1: Machine Typing Effect
+const texts: string[] = [
+	'Front-End Developer',
+	'Back-End Developer',
+	'Content Creator',
+];
+let currentTxt = ref('');
+let currentTextIndex = ref(0);
+let addingText = ref(true);
+
+// Functon to create a typing effect
+const typeWriterEffect = () => {
+	// If we are adding text
+	if (addingText.value) {
+		if (currentTxt.value.length < texts[currentTextIndex.value].length) {
+			currentTxt.value = texts[currentTextIndex.value].substring(0, currentTxt.value.length + 1);
+			setTimeout(typeWriterEffect, 70);
+		} else {
+			// If we have finished adding text
+			addingText.value = false;
+			setTimeout(typeWriterEffect, 700);
+		}
+	} else {
+		// If we are removing text
+		if (currentTxt.value.length > 0) {
+			currentTxt.value = texts[currentTextIndex.value].substring(0, currentTxt.value.length - 1);
+			setTimeout(typeWriterEffect, 70);
+		} else {
+			// If we have finished removing text
+			addingText.value = true;
+			currentTextIndex.value = (currentTextIndex.value + 1) % texts.length;
+			setTimeout(typeWriterEffect, 300);
+		}
+	}
+};
+
+onMounted(() => {
+	typeWriterEffect();
+});
 </script>
 
 <template>
-  <div class="h-screen px-8 pt-20 border overflow-y-none">
-    <div class="h-full pt-20 space-y-8 leading-snug border">
-      <h1 class="text-[8rem] font-semibold text-white leading-none break-words">
-        Hello Everyone!
-      </h1>
-      <h2 class="text-[6rem] font-semibold text-white leading-none whitespace-normal">
-        Welcome to my website!
-      </h2>
-    </div>
-  </div>
+  <PageCover
+    background-type="clip"
+    :background-url="backgroundClip"
+    overlay-color="black"
+  >
+    <template #cover-content>
+      <div
+        class="flex flex-col h-screen transition-all duration-300 ease-in-out"
+        :class="{
+          'justify-center items-start': !xs && !sm && !md,
+          'justify-center items-center': xs || sm || md,
+        }"
+      >
+        <span
+          class="leading-none text-white whitespace-normal font-bebas"
+          :class="{
+            'text-[2.5rem] text-center': xs || sm,
+            'text-[5rem] text-center': md,
+            'text-[6rem] text-left': !xs && !sm && !md,
+          }"
+        >
+          Hello Everyone!
+        </span>
+        <span
+          class="font-bebas text-[8rem] text-white leading-tight transition-all duration-300 ease-in-out whitespace-normal"
+          :class="{
+            'text-[3.5rem] text-center': xs || sm,
+            'text-[6rem] text-center': md,
+            'text-[7rem] text-left': !xs && !sm && !md,
+
+          }"
+        >
+          I'm Stefano Biddau
+        </span>
+        <span
+          class="font-bebas text-[6rem] text-white leading-none transition-all duration-300 ease-in-out whitespace-normal "
+          :class="{
+            'text-[2.5rem] text-center': xs || sm,
+            'text-[5rem] text-center': md,
+            'text-[6rem] text-left': !xs && !sm && !md,
+          }"
+        >
+          I'm a {{ currentTxt }}_
+        </span>
+      </div>
+    </template>
+  </PageCover>
 </template>
