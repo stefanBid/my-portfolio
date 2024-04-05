b0fb0d1e2d6658b31483bda1ae2e8a6fdf4e7f9d
stefanBid <biddau.stefano99@gmail.com>
2024-04-05 12:00:13 +0200

    re-organize component section

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,10 @@
 // ATOMS
 export { default as CustomButton } from '@/components/atoms/custom-button/CustomButton.vue';
-export { default as PageHeading } from '@/components/atoms/page-heading/PageHeading.vue';
-export { default as PageCover } from '@/components/atoms/page-cover/PageCover.vue';
+
 // MOLECULES
+export { default as PageHeading } from '@/components/molecules/page-heading/PageHeading.vue';
+export { default as PageCover } from '@/components/molecules/page-cover/PageCover.vue';
+export { default as VintageImageContainer } from '@/components/molecules/vintage-image-container/VintageImageContainer.vue';
 
 // ORGANISMS
 export { default as MainHeader } from '@/components/organisms/main-header/MainHeader.vue';
