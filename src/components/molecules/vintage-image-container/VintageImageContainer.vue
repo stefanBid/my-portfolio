84604fa37a4f90a7ecd78a6aae245dd4c5fe9dd5
stefanBid <biddau.stefano99@gmail.com>
2024-04-08 19:31:58 +0200

    feat add biography data and add view effects

--- src/components/molecules/vintage-image-container/VintageImageContainer.vue
+++ src/components/molecules/vintage-image-container/VintageImageContainer.vue
@@ -14,14 +14,14 @@ const props = withDefaults(defineProps<VintageImageContainerProps>(), {
 <template>
   <div
     v-bind="$attrs"
-    class="px-8 pt-8 pb-20 bg-white rounded"
+    class=" px-4 pt-4 bg-white rounded w-[23.06rem] h-[28rem] overflow-hidden"
   >
     <img
       :src="props.imageUrl"
       alt="Placeholder"
-      class="object-cover object-center size-96 "
+      class="object-cover object-center w-full h-[20rem]"
     />
-    <p class="w-full mt-4 text-lg text-center whitespace-normal text-secondary font-roboto">
+    <p class="mt-4 text-sm text-center truncate whitespace-normal border text-secondary font-roboto">
       {{ props.text }}
     </p>
   </div>
--- src/views/about-page/AboutPage.vue
+++ src/views/about-page/AboutPage.vue
@@ -1,43 +1,74 @@
 <script setup lang="ts">
 import { PageHeading, VintageImageContainer } from '@/components';
-import img from '@/assets/media/graduationPhoto.jpg';
+import { vIntersectionObserver } from '@vueuse/components';
+import { useGlobalBreakpoints } from '@/hooks';
+
+import bio from '@/data/bio.json';
+import { ref } from 'vue';
+
+const initializeBooleanArray = (structure: {[key:string]:string}[]) => {
+	let arr: boolean[] = [];
+	structure.forEach(() => arr.push(false));
+	return arr;
+};
+
+const root = ref(null);
+const isVisible = ref<boolean[]>(initializeBooleanArray(bio.sections));
+
+const onIntersectionObserver = (index: number) => ([{ isIntersecting }]: IntersectionObserverEntry[]) => {
+	isVisible.value[index] = isIntersecting;
+};
+
+const { xs, sm, md } = useGlobalBreakpoints();
 
 </script>
 
 <template>
-  <div class="div_container">
+  <div
+    ref="root"
+    class=" div_container"
+  >
     <PageHeading
       title="Who am I?"
     />
 
-    <div class="flex flex-col mt-20 border gap-y-20">
+    <div
+      :class="
+        {
+          'gap-y-28': xs || sm || md,
+          'gap-y-20': !xs && !sm && !md,
+        }
+      "
+      class="flex flex-col mt-5 mb-10 "
+    >
       <div
-        v-for="i in 3"
-        :key="i"
+        v-for="(section, index) in bio.sections"
+        :key="index"
+        v-intersection-observer="[onIntersectionObserver(index), {root, threshold: 0.2}]"
         :class="{
-          'flex-row': i % 2 === 0,
-          'flex-row-reverse': i % 2 !== 0,
+          'flex-row gap-x-10': index % 2 === 0 && !xs && !sm && !md,
+          'flex-row-reverse gap-x-10': index % 2 !== 0 && !xs && !sm && !md,
+          'flex-col items-center gap-y-5': xs || sm || md,
+          'opacity-0': !isVisible[index],
+          'opacity-100': isVisible[index],
         }"
-        class="flex px-8 gap-x-10"
+        class="flex px-8 transition-all duration-300 ease-in-out"
       >
         <VintageImageContainer
-          :image-url="img"
-          text="Graduation Day 15-12-2023"
+          :image-url="section.image"
+          :text="section.imageDescription"
           :class="{
-            '-rotate-2': i % 2 === 0,
-            'rotate-2': i % 2 !== 0,
+            '-rotate-2': index % 2 === 0,
+            'rotate-2': index % 2 !== 0,
           }"
           class="shrink-0 h-fit"
         />
         <div class="flex flex-col justify-center flex-1">
           <h1 class="font-bebas text-[3rem] text-white">
-            About my journey
+            {{ section.title }}
           </h1>
           <p class="text-justify text-white whitespace-normal font-roboto">
-            Nostrud consectetur id anim amet quis amet ipsum aliquip ad veniam. Est aliquip culpa deserunt sunt qui do excepteur ullamco est irure proident deserunt consectetur. Veniam mollit cillum voluptate laborum occaecat nisi ut sint occaecat nostrud qui. Amet commodo exercitation aute laboris esse nisi sunt sit dolor qui ad anim Lorem adipisicing. Duis consectetur minim laboris enim amet nisi proident. Dolore nostrud occaecat proident in esse. Consectetur dolor dolor aliquip reprehenderit nisi amet culpa eiusmod excepteur est.
-            Nostrud consectetur id anim amet quis amet ipsum aliquip ad veniam. Est aliquip culpa deserunt sunt qui do excepteur ullamco est irure proident deserunt consectetur. Veniam mollit cillum voluptate laborum occaecat nisi ut sint occaecat nostrud qui. Amet commodo exercitation aute laboris esse nisi sunt sit dolor qui ad anim Lorem adipisicing. Duis consectetur minim laboris enim amet nisi proident. Dolore nostrud occaecat proident in esse. Consectetur dolor dolor aliquip reprehenderit nisi amet culpa eiusmod excepteur est.
-            Nostrud consectetur id anim amet quis amet ipsum aliquip ad veniam. Est aliquip culpa deserunt sunt qui do excepteur ullamco est irure proident deserunt consectetur. Veniam mollit cillum voluptate laborum occaecat nisi ut sint occaecat nostrud qui. Amet commodo exercitation aute laboris esse nisi sunt sit dolor qui ad anim Lorem adipisicing. Duis consectetur minim laboris enim amet nisi proident. Dolore nostrud occaecat proident in esse. Consectetur dolor dolor aliquip reprehenderit nisi amet culpa eiusmod excepteur est.
-            Nostrud consectetur id anim amet quis amet ipsum aliquip ad veniam. Est aliquip culpa deserunt sunt qui do excepteur ullamco est irure proident deserunt consectetur. Veniam mollit cillum voluptate laborum occaecat nisi ut sint occaecat nostrud qui. Amet commodo exercitation aute laboris esse nisi sunt sit dolor qui ad anim Lorem adipisicing. Duis consectetur minim laboris enim amet nisi proident. Dolore nostrud occaecat proident in esse. Consectetur dolor dolor aliquip reprehenderit nisi amet culpa eiusmod excepteur est.
+            {{ section.description }}
           </p>
         </div>
       </div>
