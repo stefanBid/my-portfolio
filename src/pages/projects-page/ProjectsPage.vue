3e452ae8e58ede93f4a2d4074d54b62200aef1c4
Stefano <biddau.stefano99@gmail.com>
2024-08-23 17:04:57 +0200

    fix project page

--- src/pages/projects-page/ProjectsPage.vue
+++ src/pages/projects-page/ProjectsPage.vue
@@ -8,22 +8,20 @@ import { useTypedI18nSingleton, useIntersectionObserver } from '@/hooks';
 const { currentLanguage } = useTypedI18nSingleton();
 
 // Feature 3: Manage Visibility for effects
-const { root, isVisible, onIntersectionObserver, vIntersectionObserver } = useIntersectionObserver(1);
+const { isVisible, onIntersectionObserver, vIntersectionObserver } = useIntersectionObserver(1);
 
 </script>
 
 <template>
-  <ThePageContainer
-    ref="root"
-  >
+  <ThePageContainer>
     <template #intro-section>
       <BaseHero
         :title="currentLanguage === 'en' ? 'My projects' : 'I miei progetti'"
       />
     </template>
-    <template #main-content>
+    <template #page-content>
       <PageUnderConstructionAlert
-        v-intersection-observer="[onIntersectionObserver(0), {root, threshold: 0.5}]"
+        v-intersection-observer="[onIntersectionObserver(0), {root: null, threshold: 0.5}]"
         class="transition-all duration-500 ease-in-out"
         :class="{
           'opacity-0': !isVisible[0],
