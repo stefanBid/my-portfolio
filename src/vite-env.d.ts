d34fe17665ce7408621e5df1ce5610fefac6b89f
Stefano <biddau.stefano99@gmail.com>
2024-11-23 14:59:29 +0100

    fix: remove unnecessary blank line in vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-svg-loader" />
 
-
 interface ImportMetaEnv {
   readonly VITE_EMAILJS_SERVICE_ID: string;
   readonly VITE_EMAILJS_TEMPLATE_ID: string;
