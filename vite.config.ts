63ea32d703b60513fbb0a4773551c61856bb2f9e
Stefano <biddau.stefano99@gmail.com>
2024-11-23 13:27:39 +0100

    fix: update version to 1.7.0 and adjust Node.js version in CI workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,14 @@
-import { defineConfig, type PluginOption } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
 import path from 'path';
-import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  const plugins: PluginOption[] = [
-    vue(),
-    svgLoader(),
-    viteCompression({
-      algorithm: 'gzip',
-      ext: '.gz',
-      deleteOriginFile: false,
-      threshold: 1,
-    }),
-  ];
-
-  return {
-    plugins,
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+export default defineConfig({
+  plugins: [vue(), svgLoader()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    build: {
-      rollupOptions: {
-        output: {
-          // eslint-disable-next-line consistent-return
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return id.toString().split('node_modules/')[1].split('/')[0].toString();
-            }
-          },
-        },
-      },
-    },
-  };
+  },
 });
