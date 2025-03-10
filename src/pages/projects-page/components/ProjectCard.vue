c96f87d7ebdc8c12c59992b3fed52f326bf575fd
Stefano <biddau.stefano99@gmail.com>
2025-03-10 16:18:24 +0100

    feat: update ProjectCard styling for improved visibility and hover effects

--- src/pages/projects-page/components/ProjectCard.vue
+++ src/pages/projects-page/components/ProjectCard.vue
@@ -13,7 +13,7 @@ const styleStore = useStyleStore();
     class="box-border relative flex flex-col items-center justify-center w-full transition-all duration-300 ease-in-out border-2 rounded-lg outline-none h-72 border-sb-secondary-200 group ring-0 focus-visible:ring-0 focus-visible:outline-none hover:scale-105 focus-visible:scale-105"
   >
     <!--Black Filter-->
-    <div class="absolute inset-0 rounded-md bg-black/40 z-sb-base-2"></div>
+    <div class="absolute inset-0 rounded-md bg-black/60 z-sb-base-2"></div>
     <img
       class="absolute object-cover transition-all duration-300 ease-in-out rounded-md size-full z-sb-base-1 grayscale group-hover:grayscale-0 group-focus-visible:grayscale-0"
       src="https://picsum.photos/1000"
@@ -33,7 +33,7 @@ const styleStore = useStyleStore();
             styleStore.activeBreakpoint !== 'md',
         },
       ]"
-      class="absolute text-center text-white transition-all duration-300 ease-in-out font-bebas z-sb-base-3"
+      class="absolute text-center text-white transition-all duration-300 ease-in-out group-hover:text-sb-tertiary-100 group-focus-visible:text-sb-tertiary-100 font-bebas z-sb-base-3"
     >
       Test
     </h4>
