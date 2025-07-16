b79ab7e46f7931e78604ea2dbac37d80ead540e0
stefanBid <biddau.stefano99@gmail.com>
2025-07-16 11:31:04 +0200

    fix: update package version to 6.0.1 and adjust icon import names for consistency

--- src/components/base/icon/iconsMap.ts
+++ src/components/base/icon/iconsMap.ts
@@ -1,54 +1,54 @@
-import IconCircleFlagsIt from '~icons/circle-flags/it';
-import IconCircleFlagsGb from '~icons/circle-flags/gb';
+import CircleFlagsIt from '~icons/circle-flags/it';
+import CircleFlagsGb from '~icons/circle-flags/gb';
 
-import IconDeviconJavascript from '~icons/devicon/javascript';
-import IconDeviconTypescript from '~icons/devicon/typescript';
-import IconDeviconHtml5 from '~icons/devicon/html5';
-import IconDeviconCss3 from '~icons/devicon/css3';
-import IconDeviconVuejs from '~icons/devicon/vuejs';
-import IconDeviconReact from '~icons/devicon/react';
-import IconDeviconAngular from '~icons/devicon/angular';
-import IconDeviconJava from '~icons/devicon/java';
-import IconDeviconPython from '~icons/devicon/python';
-import IconDeviconCsharp from '~icons/devicon/csharp';
-import IconDeviconSpring from '~icons/devicon/spring';
-import IconDeviconHibernate from '~icons/devicon/hibernate';
-import IconDeviconMysql from '~icons/devicon/mysql';
-import IconDevApollographql from '~icons/devicon/apollographql';
+import DeviconJavascript from '~icons/devicon/javascript';
+import DeviconTypescript from '~icons/devicon/typescript';
+import DeviconHtml5 from '~icons/devicon/html5';
+import DeviconCss3 from '~icons/devicon/css3';
+import DeviconVuejs from '~icons/devicon/vuejs';
+import DeviconReact from '~icons/devicon/react';
+import DeviconAngular from '~icons/devicon/angular';
+import DeviconJava from '~icons/devicon/java';
+import DeviconPython from '~icons/devicon/python';
+import DeviconCsharp from '~icons/devicon/csharp';
+import DeviconSpring from '~icons/devicon/spring';
+import DeviconHibernate from '~icons/devicon/hibernate';
+import DeviconMysql from '~icons/devicon/mysql';
+import DevApollographql from '~icons/devicon/apollographql';
 
-import IconFa6BrandsGithub from '~icons/fa6-brands/github';
-import IconFa6BrandsLinkedin from '~icons/fa6-brands/linkedin';
-import IconFa6BrandsInstagram from '~icons/fa6-brands/instagram';
-import IconFa6BrandsNpm from '~icons/fa6-brands/npm';
+import Fa6BrandsGithub from '~icons/fa6-brands/github';
+import Fa6BrandsLinkedin from '~icons/fa6-brands/linkedin';
+import Fa6BrandsInstagram from '~icons/fa6-brands/instagram';
+import Fa6BrandsNpm from '~icons/fa6-brands/npm';
 
 import MdiEmail from '~icons/mdi/email';
 import MdiPhone from '~icons/mdi/phone';
 
 export const ICONS_MAP = {
   // Flag icons
-  'circle-flags:it': IconCircleFlagsIt,
-  'circle-flags:gb': IconCircleFlagsGb,
+  'circle-flags:it': CircleFlagsIt,
+  'circle-flags:gb': CircleFlagsGb,
   // Language icons
-  'devicon:javascript': IconDeviconJavascript,
-  'devicon:typescript': IconDeviconTypescript,
-  'devicon:html5': IconDeviconHtml5,
-  'devicon:css3': IconDeviconCss3,
-  'devicon:vuejs': IconDeviconVuejs,
-  'devicon:react': IconDeviconReact,
-  'devicon:angular': IconDeviconAngular,
-  'devicon:java': IconDeviconJava,
-  'devicon:python': IconDeviconPython,
-  'devicon:csharp': IconDeviconCsharp,
-  'devicon:spring': IconDeviconSpring,
-  'devicon:hibernate': IconDeviconHibernate,
-  'devicon:mysql': IconDeviconMysql,
-  'devicon:apollographql': IconDevApollographql,
+  'devicon:javascript': DeviconJavascript,
+  'devicon:typescript': DeviconTypescript,
+  'devicon:html5': DeviconHtml5,
+  'devicon:css3': DeviconCss3,
+  'devicon:vuejs': DeviconVuejs,
+  'devicon:react': DeviconReact,
+  'devicon:angular': DeviconAngular,
+  'devicon:java': DeviconJava,
+  'devicon:python': DeviconPython,
+  'devicon:csharp': DeviconCsharp,
+  'devicon:spring': DeviconSpring,
+  'devicon:hibernate': DeviconHibernate,
+  'devicon:mysql': DeviconMysql,
+  'devicon:apollographql': DevApollographql,
 
   // Social icons
-  'fa6-brands:github': IconFa6BrandsGithub,
-  'fa6-brands:linkedin': IconFa6BrandsLinkedin,
-  'fa6-brands:instagram': IconFa6BrandsInstagram,
-  'fa6-brands:npm': IconFa6BrandsNpm,
+  'fa6-brands:github': Fa6BrandsGithub,
+  'fa6-brands:linkedin': Fa6BrandsLinkedin,
+  'fa6-brands:instagram': Fa6BrandsInstagram,
+  'fa6-brands:npm': Fa6BrandsNpm,
 
   // Other icons
   'mdi:email': MdiEmail,
