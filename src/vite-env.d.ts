27af2301c503dd95d1d2277a3a51cad53a259d2c
stefanBid <biddau.stefano99@gmail.com>
2025-09-20 23:42:08 +0200

    fix: update type references in vite-env and tsconfig for improved type checking

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,1 @@
 /// <reference types="vite/client" />
-/// <reference types="vite-svg-loader" />
-
-interface ImportMetaEnv {
-  readonly VITE_EMAILJS_SERVICE_ID: string;
-  readonly VITE_EMAILJS_TEMPLATE_ID: string;
-  readonly VITE_EMAILJS_PUBLIC_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
